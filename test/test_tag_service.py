7a15dd39a47e825f16a00fc2535e99b0420e38a2
Carlson <carlson.t.smith@gmail.com>
2020-07-20 11:19:05 -0700

    tagging/tag_service.py

--- test/test_tag_service.py
+++ test/test_tag_service.py
@@ -17,91 +17,211 @@ def tag_svc(mongodb):
 
 
 def test_unique_uid_tag_set(tag_svc):
-    tagging_event_uid = tag_svc.create_tagging_event(tagging_event)
-    tag_svc.create_tag_set(test_tags, tagging_event_uid)
+    tagger_uid = tag_svc.create_tagger(tagger)
+    tagging_event_uid = tag_svc.create_tagging_event(tagging_event, tagger_uid)
+    asset_tags_uid = tag_svc.create_asset_tags(asset_tags, tagging_event_uid)
     with pytest.raises(DuplicateKeyError):
-        tag_svc.create_tag_set({'uid': test_tags['uid'], 'tags': [{'key': 'foo', 'value': 'bar'}]}, tagging_event_uid)
-
+        tag_svc.create_tagger({'uid': tagger['uid'], "type": "model",
+                "model_name": "netty", "create_time": 2})
+    with pytest.raises(DuplicateKeyError):
+        tag_svc.create_tagging_event({'uid': tagging_event['uid'], "tagger_id":
+            tagger['uid'],"run_time": 3, "accuracy": 1}, tagger_uid)
+    with pytest.raises(DuplicateKeyError):
+        tag_svc.create_asset_tags({'uid': asset_tags['uid'], "sample_id": "plants", "tags": []},
+            tagging_event_uid)
 
 def test_random_sets(tag_svc):
-    tagging_event_uid = tag_svc.create_tagging_event(tagging_event)
+    tagger_uid = tag_svc.create_tagger(tagger)
     for x in range(10):
-        tag_svc.create_tag_set({'tags': [{'key': 'foo', 'value': 'bar'}]}, tagging_event_uid)
-    cursor = tag_svc.find_random_tag_sets(3)
+        tagging_event_uid = tag_svc.create_tagging_event({'uid': None, "tagger_id":
+                None, "run_time": 3, "accuracy": 1}, tagger_uid)
+        asset_tags_uid = tag_svc.create_asset_tags({'uid': None, "sample_id":
+                "plants", "tags": []}, tagging_event_uid)
+    cursor = tag_svc.find_random_event_sets(3)
+    assert count_results(cursor) == 3
+    cursor = tag_svc.find_random_asset_sets(3)
     assert count_results(cursor) == 3
 
 
-def test_get_tags(tag_svc):
-    tagging_event_uid = tag_svc.create_tagging_event(tagging_event)
-    tag_svc.create_tag_set({'tags': [
-                                {'key': 'frame_material', 'value': 'steel'},
-                                {'key': 'color', 'value': 'red'},
-                                {'key': 'num_spokes', 'value': 36}]}, tagging_event_uid)
+def test_get_events_and_tags(tag_svc):
+    tagger_uid = tag_svc.create_tagger(tagger)
 
-    tag_svc.create_tag_set({'tags': [
-                                {'key': 'frame_material', 'value': 'steel'},
-                                {'key': 'color', 'value': 'green'},
-                                {'key': 'num_spokes', 'value': 36}]}, tagging_event_uid)
+    tagging_event_uid = tag_svc.create_tagging_event({
+            'uid': None,
+            'tagger_id': None,
+            'run_time': 1134433.223,
+            'accuracy': 0.7776}, tagger_uid)
+    asset_tags_uid = tag_svc.create_asset_tags({
+            'uid': None,
+            'event_id': None,
+            'sample_id': 'house paint 1234',
+            'tags': [{
+                    'tag': 'rods',
+                    'confidence': 0.908,
+                    'event_id': None,
+                },{
+                    'tag': 'giwaxs',
+                    'confidence': 0.092, 
+                    'event_id': None,
+                }]}, tagging_event_uid)
 
-    tag_svc.create_tag_set({'tags': [
-                                {'key': 'frame_material', 'value': 'carbon'},
-                                {'key': 'num_spokes', 'value': 3}]}, tagging_event_uid)
 
-    tag_svc.create_tag_set({'tags': [
-                                {'key': 'color', 'value': 'red'},
-                                {'key': 'num_spokes', 'value': 24}]}, tagging_event_uid)
+    tagging_event_uid = tag_svc.create_tagging_event({
+            'uid': None,
+            'tagger_id': None,
+            'run_time': 0,
+            'accuracy': 1}, tagger_uid)
+    asset_tags_uid = tag_svc.create_asset_tags({
+            'uid': None,
+            'sample_id': 'paint',
+            'tags': [{
+                    'tag': 'beamline',
+                    'confidence': 0.998,
+                    'event_id': None,
+                },{
+                    'tag': 'giwaxs',
+                    'confidence': 0.002, 
+                    'event_id': None,
+                }]}, tagging_event_uid)
 
-    cursor = tag_svc.find_tag_sets_one_filter('color', 'red')
+     
+    tagging_event_uid = tag_svc.create_tagging_event({
+            'uid': None,
+            'tagger_id': None,
+            'run_time': 23,
+            'accuracy': 0}, tagger_uid)
+    asset_tags_uid = tag_svc.create_asset_tags({
+            'uid': None,
+            'sample_id': 'house',
+            'tags': [{
+                    'tag': 'rings',
+                    'confidence': 0.998,
+                    'event_id': None,
+                },{
+                    'tag': 'giwaxs',
+                    'confidence': 0.002, 
+                    'event_id': None,
+                }]}, tagging_event_uid)
+
+
+    cursor = tag_svc.find_tags_one_filter('giwaxs', 0.002)
     assert count_results(cursor) == 2
 
-    mongo_filter = [{'tags.key': 'num_spokes'}, {'tags.value': {'$gt': 3}}]
-    cursor = tag_svc.find_tag_sets_mongo(mongo_filter)
+    mongo_filter = {'tags.tag': 'giwaxs', 'tags.confidence': {'$gt': 0}}
+    cursor = tag_svc.find_asset_sets_mongo(mongo_filter)
     assert count_results(cursor) == 3
 
+    mongo_filter = {'run_time': {'$gt': 0}}
+    cursor = tag_svc.find_events_mongo(mongo_filter)
+    assert count_results(cursor) == 2 
+
+    mongo_filter = {'accuracy': {'$gt': 0}}
+    cursor = tag_svc.find_events_mongo(mongo_filter)
+    assert count_results(cursor) == 2 
+    
+    mongo_filter = {'type': 'model'}
+    cursor = tag_svc.find_tagger_mongo(mongo_filter)
+    assert count_results(cursor) == 1
 
 def count_results(cursor):
     counter = 0
-    for tag_set in cursor:
+    for data_set in cursor:
         counter += 1
     return counter
 
-
-def test_tag_event(tag_svc):
-    tagging_event_uid = tag_svc.create_tagging_event(tagging_event)
-    assert tagging_event_uid is not None
-    return_tagging_event = tag_svc.get_tagging_event(tagging_event_uid)
-    assert return_tagging_event.get('schema_version') == TagService.SCHEMA_VERSION
+def test_tagger(tag_svc):
+    tagger_uid = tag_svc.create_tagger(tagger)
+    assert tagger_uid is not None
+    return_tagger = tag_svc.get_tagger(tagger_uid)
+    assert return_tagger.get('schema_version') == TagService.SCHEMA_VERSION
 
 
 def test_tag_set(tag_svc):
-    tagging_event_uid = tag_svc.create_tagging_event(tagging_event)
-    tag_svc.create_tag_set(test_tags, tagging_event_uid)
-    return_tag_set = tag_svc.find_tag_set(test_tags['uid'])
-    assert return_tag_set.get('schema_version') == TagService.SCHEMA_VERSION
+    tagger_uid = tag_svc.create_tagger(tagger)
+    tagging_event_uid = tag_svc.create_tagging_event(tagging_event, tagger_uid)
+    tag_svc.create_asset_tags(asset_tags, tagging_event_uid)
+    return_asset_set = tag_svc.find_asset_set(asset_tags['uid'])
+    assert return_asset_set.get('schema_version') == TagService.SCHEMA_VERSION
+   
+    for tags in asset_tags['tags']:
+        assert tags['event_id'] == tagging_event_uid
+
+    return_asset_set2 = tag_svc.find_asset_set(return_asset_set['uid'])
+    assert return_asset_set2['uid'] == return_asset_set['uid']
+
+def test_add_asset_tags(tag_svc):
+    tagger_uid = tag_svc.create_tagger(tagger)
+    tagging_event_uid = tag_svc.create_tagging_event(tagging_event, tagger_uid)
+    asset_tags_uid = tag_svc.create_asset_tags(asset_tags, tagging_event_uid)
     
-    for tag in return_tag_set['tags']:
-        assert tag['tag_event'] == tagging_event_uid
+    return_asset_set = tag_svc.add_asset_tags([{
+            "tag": "add1",
+            "confidence": 0.50,
+            "event_id": "wwewere6002104rwerwe81ad229c33",
+        },{
+            "tag": "add2",
+            "confidence": 0.50, 
+            "event_id": "wwewere6002104rwerwe81ad229c33",
+        }], asset_tags_uid)
 
-    return_tag_set = tag_svc.find_tag_set(return_tag_set['uid'])
-    assert return_tag_set['uid'] == return_tag_set['uid']
+    assert len(return_asset_set['tags']) == 4
 
 
-test_tags = {
- "asset_uid": "ee600210432b8f81ad229c33",
- "tags": [
-   {
-     "key": "scattering_geometry",
-     "value": "transmission",
-     "confidence": 0.9008,
-   },
-   {
-     "key": "sample_detector_distance_name",
-     "value": "WAXS",
-     "confidence": 0.001, 
-   }
- ],
+
+
+# this was test_tags before
+asset_tags = {
+    "uid": None,
+    "sample_id": "house paint 1234",
+    "tags": [
+        {
+            "tag": "rods",
+            "confidence": 0.9008,
+            "event_id": None,
+        },
+        {
+            "tag": "peaks",
+            "confidence": 0.001, 
+            "event_id": None,
+        }
+    ]
 }
-
+# name didnt change, though it doesnt carry model_name anymore
 tagging_event = {
-    "model_name": "PyTestNet"
+    "uid": None,
+    "tagger_id": None,
+    "run_time": 1134433.223,
+    "accuracy": 0.7776
+    
 }
+
+# takes place of tagging_event as the parent dir
+tagger = {
+    "uid": None,
+    "type": "model",
+    "model_name": "PyTestNet",
+    "create_time": 11112333.3
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
